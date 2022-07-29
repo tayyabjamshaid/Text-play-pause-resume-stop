149ef06a7a6d94432d2c737f07c2da66c81eab44
tayyabjamshaid <tayyabjamshaid78@gmail.com>
2022-07-29 00:45:09 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { useState } from "react";
+//https://developer.mozilla.org/en-US/docs/Web/API/SpeechSynthesis/pause
+//See this documentation to add more functionalities
 function App() {
+  const [ourText, setOurText] = useState("");
+  const msg = new SpeechSynthesisUtterance();
+
+  const speechHandler = (msg) => {
+    msg.text = ourText;
+    window.speechSynthesis.speak(msg);
+  };
+  const speechStopHandler = () => {
+    window.speechSynthesis.cancel();
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>React Text to Speech App</h1>
+      <input
+        type="text"
+        value={ourText}
+        placeholder="Enter Text"
+        onChange={(e) => setOurText(e.target.value)}
+      />
+      <button onClick={() => speechHandler(msg)}>SPEAK</button>
+      <button onClick={() => speechStopHandler()}>Stop</button>
     </div>
   );
 }
